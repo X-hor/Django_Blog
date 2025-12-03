a8ebe67144ad856ae5c8213b116c22632cab0da9
LACHAB ABDELNOUR <lachab.abdelnour@gmail.com>
2025-12-03 12:50:07 +0100

    Create superuser from environment variables

--- build.sh
+++ build.sh
@@ -4,3 +4,17 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser from environment variables
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'changeme')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('Superuser created')
+END
